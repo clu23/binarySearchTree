b2dcd4d0bdd22c5e5fdd1bb812d78c97c9ee8403
clu23 <clubuntu@FRTOU-61P2C14.eu.hexagonmetrology.com>
2024-08-24 15:50:29 +0200

    adding Node and BinarySearchTree classes with their constructors

---
+++ binarySearchTree.js
@@ -0,0 +1,19 @@
+
+
+class Node{
+
+    constructor(value){
+        this.value=value;
+        this.rigth=null;
+        this.left=null;
+    }
+    
+}
+
+
+class BinarySearchTree{
+
+    constructor(){
+        this.root=null;
+    }
+}
